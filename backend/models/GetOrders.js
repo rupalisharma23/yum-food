91acf9254b1f6f484e83b7a44dfba1296099614e
rupalisharma23 <rupalisharma232002@gmail.com>
2023-07-02 15:10:09 +0530

    deleted backend

--- backend/models/GetOrders.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const getOrders = new Schema({
-  orders: [{ name: String, price: String, quantity: Number }],
-});
-
-module.exports = mongoose.model("orders",getOrders)
