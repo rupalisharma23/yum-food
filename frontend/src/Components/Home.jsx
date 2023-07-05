a2b008020e202a0c5ceacd63547fde7b2ff5424e
rupalisharma23 <rupalisharma232002@gmail.com>
2023-07-05 23:47:21 +0530

    changes

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -10,6 +10,7 @@ import backendURL from './Config';
 
 export default function Home() {
     const [cartCount, setCartCount] = useState('')
+    const [search, setSearch] = useState('')
     const [food, setFood] = useState([])
     const [cartFlag, setCartFlag] = useState(false)
     const currentDate = new Date();
@@ -123,8 +124,24 @@ export default function Home() {
             <div className="image-container">
                 <img src="/background.jpg" alt="Image" />
             </div>
+            <div className="search-bar-container">
+                <div className="search-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                    <input type="text" placeholder="Search by categories ..." onChange={(e) => { setSearch(e.target.value) }} />
+                </div>
+
+                
+            </div>
             {
-                food.map((data, index1) => {
+                food.filter((val) => {
+                    if (search === '') {
+                        return val;
+                    } else if (val.categoryName.toLowerCase().includes(search.toLowerCase())) {
+                        return val;
+                    }
+                }).map((data, index1) => {
                     return (
                         <>
                             <div className='category'>{data.categoryName}</div>
